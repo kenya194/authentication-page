3888f38ac3a09602561b96c00fb613d70c0208a9
kenya194 <eamankwah698@gmail.com>
2023-10-17 23:13:15 -0700

    implementing states

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import './signup.css'
 import {BiUser} from 'react-icons/bi'
 import {AiOutlineMail} from 'react-icons/ai'
@@ -6,12 +7,17 @@ import {RiLockPasswordFill} from 'react-icons/ri'
 
 
 const Signup = () => {
+
+  const[action, setAction] = useState('Login');
+
+
   return (
     <div className='container'>
         <div className='header'>
-          <h1>Sign up</h1>
+          <div className='text'> {action} </div>
+          <div className='underline'></div>
         </div>
-    <div className='inputs'>
+      <div className='inputs'>
         <div className='input'>
           <BiUser />
           <input type='text' placeholder='Enter Name'/>
@@ -30,12 +36,12 @@ const Signup = () => {
 
         <div className='password__actions'>Forgot Password? <span>click here</span></div>
 
-        <div className='button__actions'>
-          <button className='sign__up'>
+        <div className='submit-container'>
+          <button className={action === 'Sign Up'? 'submit gray':'submit'} onClick={()=>{(setAction('Sign Up'))}}>
             Signup
           </button>
           
-          <button className='log__in'>
+          <button className={action === 'Login'? 'submit gray':'submit'} onClick={()=>{(setAction('Login'))}}>
             Login
           </button>
         </div>
